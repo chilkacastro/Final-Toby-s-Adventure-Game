05b1ac92ccc2c6f80222d21a4fbc4d1f72aa3e8d
Nicolas Nguyen AKA Nicosil <nicolascsadl@gmail.com>
2020-11-26 19:15:39 -0500

    Implemented basic structure for Sandbox Level 4 that involves mechanics from other levels. Added more sprites and images for special items that grants abilities

---
+++ Project/Placeholder.java
@@ -0,0 +1,48 @@
+import lang.stride.*;
+import java.util.List;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Placeholder extends Actor
+{
+    int test = 0;
+    /**
+     * Constructor
+     */
+    public Placeholder()
+    {
+        //test = Greenfoot.getRandomNumber(50);
+    }
+
+    /**
+     * Moves towards left screen
+     */
+    public void act()
+    {        
+        Actor toby = getOneObjectAtOffset(0, 0, Toby.class);
+        if (toby != null) {
+            World theWorld = getWorld();
+            theWorld.removeObject(toby);
+            Greenfoot.playSound("oof.wav");
+            Greenfoot.stop();
+            getWorld().showText("POISONED!", 300, 200);
+        }
+        moveAndTurn(); 
+    }
+
+    /**
+     * 
+     */
+    public void moveAndTurn()
+    {
+        move(3);
+        if (Greenfoot.getRandomNumber(100) < 10) {
+            turn(Greenfoot.getRandomNumber(30) - 15);
+        }
+        if(isAtEdge()) {
+            turn(180);
+        }
+    }
+}
