ef4f02cca3133ae4d2f2fb9ec695fff446e94bbb
Turcan Madalina <turcan.madalina@yahoo.com>
2020-11-29 18:29:00 -0500

    add images

---
+++ Project/DogCatcher2.java
@@ -0,0 +1,105 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class DogCatcher here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DogCatcher2 extends Actor{
+    private GreenfootImage image1;
+    private GreenfootImage image2;
+    private GreenfootImage image3;
+    private GreenfootImage image4;
+    private int counter1=0;
+    private int counter2=0;
+    private int counter3= 0;
+    
+    public DogCatcher2(){
+        image1 = new GreenfootImage("rightcatcher1.png");
+        image2 = new GreenfootImage("rightcatcher2.png");
+        image3 = new GreenfootImage("leftcatcher1.png");
+        image4 = new GreenfootImage("leftcatcher2.png");       
+    }
+    /**
+     * Act - do whatever the DogCatcher wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+    movement();
+    if(isTouching(Toby.class)) {
+        Greenfoot.playSound("Crying.wav");
+        removeTouching(Toby.class);
+        getWorld().showText("You have been CAPTURED AND ENSLAVED BY THE DOG CATCHER!",500, 500);
+        Greenfoot.stop();
+    }
+    //atEdge();
+    //slowDown();
+    rotateAtEdge();
+    }
+  
+    
+    
+    public void movement() {
+        counter1++;    
+        if (counter1>80) {
+            setLocation(getX() + Greenfoot.getRandomNumber(5), getY());
+            counter3++;
+            if (counter3 <4) {
+                setImage (image1);
+            } else if (counter3 >8){
+            setImage(image2);
+            if (counter3 ==12){
+            counter3=0;
+            }
+                                   } 
+        }
+        else if (counter1<80) {
+            setLocation(getX() - Greenfoot.getRandomNumber(5), getY());
+            counter2++; 
+        
+        if (counter2 < 4){
+         setImage (image3);
+         }
+         else if ( counter2 > 8){
+         setImage (image4); 
+         if (counter2==12){
+         counter2= 0;
+        }
+        }
+          //    if (getImage() == image3) {
+          //  setImage(image4);
+          //  }
+          //  else {
+          //  setImage(image3);
+          //  }
+        }
+        if (counter1==100) {
+            counter1=0;
+        }
+    }    
+    
+    public void rotateAtEdge() {
+        if (getX() == 0) {
+          setLocation(1590, getY()); 
+        }
+        
+       
+    }
+    /**
+     * slowDown - to slow down Toby's movement.
+     */
+     public void slowDown(){
+    // Helps slow down the dog's catcher movement.
+    counter2++; 
+    if (counter2 < 5){
+     setImage (image3);
+     //movement();
+     }
+     else if ( counter2 > 1){
+     setImage (image4);         
+     counter2= 0;
+    }
+            }
+    }
--- Project/LeftBorder.java
+++ Project/LeftBorder.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Electric here.
+ * Write a description of class LeftBorder here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Electric extends Actor
+public class LeftBorder extends Actor
 {
     /**
-     * Act - do whatever the Electric wants to do. This method is called whenever
+     * Act - do whatever the LeftBorder wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
