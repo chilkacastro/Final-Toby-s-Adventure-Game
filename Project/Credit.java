28963c97fb0a40b811c6f22bf2965810b7593898
Turcan Madalina <turcan.madalina@yahoo.com>
2020-11-30 11:53:54 -0500

    eE

--- Project/Credit.java
+++ Project/Credit.java
@@ -17,7 +17,7 @@ public class Credit extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 560, 1); 
-<<<<<<< HEAD
+
         prepare();
     }
 
