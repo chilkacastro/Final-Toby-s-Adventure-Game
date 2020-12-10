e9fafada97292a4e75f1aabb97331adfdd7112cd
Nicolas Nguyen AKA Nicosil <nicolascsadl@gmail.com>
2020-12-10 07:37:36 -0500

    Diversified the sounds for fruits and fixed minor errors

--- Project/Power.java
+++ Project/Power.java
@@ -12,14 +12,15 @@ public class Power extends Actor
      * Act - do whatever the Power wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void givePower() 
+    public void act() 
     {
-        if (isTouching(Toby.class)) {
-            getWorld().removeObject(this);
+        //When touching Toby
+        Actor power = getOneObjectAtOffset(0, 0, TobyH.class);
+        if (power != null) {
             getWorld().showText("You have obtained the power to shoot Fireballs! Press Space to use it", 500, 500);
-            Greenfoot.playSound("Power.wav");
+            Greenfoot.playSound("Heart.wav");
+            
         }
-
     }
     
 }
